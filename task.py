850d43f5dcce753813ac650907b8954e1af7fc79
Veer1578 <sweetyatulshah@gmail.com>
2025-04-09 09:46:00 +0530

    task

---
+++ task.py
@@ -0,0 +1,11 @@
+code = int(input("Enter code "))
+if code == 1999 :
+    print("There is a traitor in team B. Find him without letting the team know because it can be anyone")
+elif code == 2422 :
+    print("The president is in danger. Keep a watch over him secretly.")
+elif code == 1832 :
+    print("There is a traitor in team A. He is off gaurd now because of wrong information. Now is the best chance to catch him.")
+elif code == 3943 :
+    print("Assasinate the minister.")
+else :
+    print("Team code not found")
